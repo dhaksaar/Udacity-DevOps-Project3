bda89f5856f966d017692747f70847dbbe340511
Daxin <daxinmoorthy@gmail.com>
2021-07-01 22:10:23 +0100

    Selenium test suite: added timestamp

--- automatedtesting/selenium/selenium_ui_test.py
+++ automatedtesting/selenium/selenium_ui_test.py
@@ -1,18 +1,19 @@
 # #!/usr/bin/env python
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
+from datetime import datetime
 
 
 # Start the browser and login with standard_user
 def login (user, password):
-    print ('Starting the browser...')
+    print (datetime.now().strftime("%Y-%m-%d %H:%M:%S") +'\tStarting the browser...')
     # --uncomment when running in Azure DevOps.
     options = ChromeOptions()
     options.add_argument("--headless") 
     options.add_argument("--no-sandbox")
     driver = webdriver.Chrome(options=options)
     #driver = webdriver.Chrome()
-    print ('Browser started successfully. Navigating to the demo page to login.')
+    print(datetime.now().strftime("%Y-%m-%d %H:%M:%S") +'\tBrowser started successfully. Navigating to the demo page to login.')
     driver.get('https://www.saucedemo.com/')
     driver.find_element_by_css_selector("input[id='user-name']").send_keys(user)
     driver.find_element_by_css_selector("input[id='password']").send_keys(password)
@@ -20,7 +21,7 @@ def login (user, password):
     inventory_label = driver.find_element_by_css_selector("div[class='inventory_item_label']").text
     print(inventory_label)
     assert "Sauce Labs Backpack" in inventory_label
-    print('Loggged in  with username {:s} and password {:s} successfully.'.format(user, password))
+    print(datetime.now().strftime("%Y-%m-%d %H:%M:%S") +'\tLoggged in  with username {:s} and password {:s} successfully'.format(user, password))
     return driver
 
 def addToCart(driver):
@@ -29,13 +30,13 @@ def addToCart(driver):
     for itemIndex in range(itemCount):
         item= driver.find_elements_by_class_name('inventory_item_name')[itemIndex]
         item.click()
-        print(driver.current_url)
+        print(datetime.now().strftime("%Y-%m-%d %H:%M:%S") +'\tNavigated to url:'+driver.current_url)
         btnAdd = driver.find_element_by_class_name('btn_inventory')
         btnAdd.click()
-        print('Added inventory {:s} to cart'.format(driver.find_element_by_class_name("inventory_details_name").text))
+        print(datetime.now().strftime("%Y-%m-%d %H:%M:%S") +'\tAdded inventory {:s} to cart'.format(driver.find_element_by_class_name("inventory_details_name").text))
         backBtn = driver.find_element_by_id('back-to-products')
         backBtn.click()
-        print(driver.current_url)
+        print(datetime.now().strftime("%Y-%m-%d %H:%M:%S") +'\tNavigated to url:'+driver.current_url)
      
 
 def removeFromCart(driver):
@@ -44,13 +45,13 @@ def removeFromCart(driver):
     for itemIndex in range(itemCount):
         item= driver.find_elements_by_class_name('inventory_item_name')[itemIndex]
         item.click()
-        print(driver.current_url)
+        print(datetime.now().strftime("%Y-%m-%d %H:%M:%S") +'\tNavigated to url:'+driver.current_url)
         btnAdd = driver.find_element_by_class_name('btn_inventory')
         btnAdd.click()
-        print('Remove inventory {:s} from cart'.format(driver.find_element_by_class_name("inventory_details_name").text))
+        print(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '\tRemove inventory {:s} from cart'.format(driver.find_element_by_class_name("inventory_details_name").text))
         backBtn = driver.find_element_by_id('back-to-products')
         backBtn.click()
-        print(driver.current_url)
+        print(datetime.now().strftime("%Y-%m-%d %H:%M:%S") +'\tNavigated to url:'+driver.current_url)
      
 
 driver = login('standard_user', 'secret_sauce')
